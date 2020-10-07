e915d07a7bb20c0d8fffdb39c79d594aad4df4f8
Amos Jun-yeung Ng <me@amos.ng>
2020-10-06 21:59:52 -0700

    Generate code for attribute, and print out generated code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+//! Yang is a code generator for Yin.
+
+#![warn(missing_docs)]
+
 use clap::clap_app;
 use zamm_yin::concepts::{ArchetypeTrait, Tao};
 use zamm_yin::wrappers::CommonNodeTrait;
@@ -5,6 +9,7 @@ use zamm_yin::wrappers::CommonNodeTrait;
 mod codegen;
 mod concepts;
 
+use codegen::handle_implementation;
 use concepts::{initialize_kb, Documentable, Implement};
 
 /// The entry-point to this code generation tool.
@@ -12,8 +17,9 @@ fn main() {
     let args = clap_app!(yang =>
         (version: "0.0.1")
         (author: "Amos Ng <me@amos.ng>")
-        (about: "Yin code generator.")
-        (@arg CONCEPT: +required "Concept name to generate code for.")
+        (about: "Code generator for Yin.")
+        (@arg CONCEPT: +required "Name of concept to generate code for.")
+        (@arg ID: -i --id +takes_value +required "ID of concept.")
         (@arg DOC: -d --documentation +takes_value "Documentation string for concept.")
     )
     .get_matches();
@@ -27,15 +33,11 @@ fn main() {
     let mut implement_command = Implement::individuate();
     implement_command.set_target(target);
 
-    match target.documentation() {
-        Some(doc) => println!(
-            "Will implement {} with documentation \"{}\".",
-            implement_command.target().unwrap().internal_name().unwrap(),
-            doc
-        ),
-        None => println!(
-            "Will implement {} without documentation.",
-            implement_command.target().unwrap().internal_name().unwrap()
-        ),
-    }
+    println!(
+        "{}",
+        handle_implementation(
+            implement_command,
+            args.value_of("ID").unwrap().parse::<usize>().unwrap()
+        )
+    );
 }
