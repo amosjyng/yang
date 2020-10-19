979d74f05a17f40948c8c967bce9f1c18128e01c
Amos Jun-yeung Ng <me@amos.ng>
2020-10-19 09:53:48 -0700

    Get example to actually run

--- examples/result/tao/mod.rs
+++ examples/result/tao/mod.rs
@@ -3,13 +3,13 @@ pub mod attribute {
     pub use target_form::Target;
 }
 
-use zamm_yin::graph::{bind_in_memory_graph, Graph, InjectionGraph};
+use zamm_yin::graph::{Graph, InjectionGraph};
 use zamm_yin::tao::archetype::ArchetypeTrait;
 use zamm_yin::tao::attribute::Inherits;
 
 pub fn initialize_kb() {
-    // Initialize Yin KB with new type
-    bind_in_memory_graph();
+    zamm_yin::tao::initialize_kb();
     let mut ig = InjectionGraph::new();
+    // Initialize Yin KB with new type
     zamm_yin::initialize_type!(ig, (attribute::Target));
 }
