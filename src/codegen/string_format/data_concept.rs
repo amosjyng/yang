eaa256eaa8da60c1b2613d09a1f0fa6cf1607aee
Amos Jun-yeung Ng <me@amos.ng>
2020-11-14 17:26:58 -0800

    Tailor Data concept generation to different Rust primitives
    Still need to generate different tests

---
+++ src/codegen/string_format/data_concept.rs
@@ -0,0 +1,102 @@
+use super::fragments::{AtomicFragment, FileFragment};
+use super::tao::{tao_fragment, tao_test_fragment};
+use super::DataFormatConfig;
+use indoc::formatdoc;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+/// Get the body fragment for a data concept.
+pub fn data_concept_fragment(cfg: &DataFormatConfig) -> AtomicFragment {
+    AtomicFragment {
+        imports: vec![
+            format!("{}::node_wrappers::BaseNodeTrait", cfg.tao_cfg.yin_crate),
+            format!("{}::graph::value_wrappers::KBValue", cfg.tao_cfg.yin_crate),
+            format!(
+                "{}::graph::value_wrappers::StrongValue",
+                cfg.tao_cfg.yin_crate
+            ),
+        ],
+        atom: formatdoc! {r#"
+            impl {name} {{
+                /// Set {primitive} value for this concept.
+                pub fn set_value(&mut self, value: {primitive}) {{
+                    self.essence_mut()
+                        .set_value(Rc::new(StrongValue::new(value)));
+                }}
+
+                /// Retrieve {primitive}-valued StrongValue.
+                pub fn value(&self) -> Option<Rc<dyn KBValue>> {{
+                    self.essence().value()
+                }}
+            }}"#, name = cfg.tao_cfg.name,
+            primitive = cfg.rust_primitive_name,
+        },
+    }
+}
+
+/// Get the string concept test fragment.
+pub fn string_concept_test_fragment(cfg: &DataFormatConfig) -> AtomicFragment {
+    AtomicFragment {
+        imports: vec![format!(
+            "{}::graph::value_wrappers::unwrap_strong",
+            cfg.tao_cfg.yin_crate
+        )],
+        atom: formatdoc! {r#"
+            #[test]
+            fn get_value_none() {{
+                initialize_kb();
+                let concept = {name}::individuate();
+                assert_eq!(unwrap_strong::<{primitive}>(&concept.value()), None);
+            }}
+        
+            #[test]
+            fn get_value_some() {{
+                initialize_kb();
+                let mut concept = {name}::individuate();
+                concept.set_value("value".to_owned());
+                assert_eq!(
+                    unwrap_strong::<{primitive}>(&concept.value()),
+                    Some(&"value".to_owned())
+                );
+            }}"#, name = cfg.tao_cfg.name,
+            primitive = cfg.rust_primitive_name,
+        },
+    }
+}
+
+/// Generate code for a string concept.
+pub fn code_data_concept(cfg: &DataFormatConfig) -> String {
+    let mut file = FileFragment::default();
+    file.append(Rc::new(RefCell::new(tao_fragment(&cfg.tao_cfg))));
+    file.append(Rc::new(RefCell::new(data_concept_fragment(cfg))));
+    let mut test_frag = tao_test_fragment(&cfg.tao_cfg);
+    test_frag.append(Rc::new(RefCell::new(string_concept_test_fragment(cfg))));
+    file.set_tests(Rc::new(RefCell::new(test_frag)));
+    file.generate_code()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_string_output() {
+        let code = code_data_concept(&DataFormatConfig {
+            rust_primitive_name: "String".to_owned(),
+            ..DataFormatConfig::default()
+        });
+        assert!(code.contains("String"));
+        assert!(!code.contains("i64"));
+    }
+
+    #[test]
+    fn test_int_output() {
+        let code = code_data_concept(&DataFormatConfig {
+            rust_primitive_name: "i64".to_owned(),
+            ..DataFormatConfig::default()
+        });
+        println!("{}", code);
+        // todo: assert no "String" in code after CommonNodeTrait gets automatically implemented
+        assert!(code.contains("i64"));
+    }
+}
---
+++ src/codegen/string_format/data_format_config.rs
@@ -0,0 +1,21 @@
+use super::FormatConfig;
+use crate::codegen::CodeConfig;
+
+/// Config values at the time of Attribute code generation.
+#[derive(Default)]
+pub struct DataFormatConfig {
+    /// Regular concept config.
+    pub tao_cfg: FormatConfig,
+    /// Rust primitive that this concept represents.
+    pub rust_primitive_name: String,
+}
+
+impl<'a> From<&'a CodeConfig<'a>> for DataFormatConfig {
+    /// Extract format values from code config.
+    fn from(cfg: &CodeConfig) -> Self {
+        Self {
+            tao_cfg: FormatConfig::from(cfg),
+            rust_primitive_name: cfg.rust_primitive_name.clone(),
+        }
+    }
+}
