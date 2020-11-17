e5f43e67cbc8c44755ce3f5f3d0be22a44477879
Amos Jun-yeung Ng <me@amos.ng>
2020-11-16 23:58:06 -0800

    Creating release v0.1.0

---
+++ src/tao/relation/attribute/implementation_name_form.rs
@@ -0,0 +1,161 @@
+use std::convert::TryFrom;
+use std::fmt;
+use std::fmt::{Debug, Formatter};
+use zamm_yin::node_wrappers::{debug_wrapper, FinalNode};
+use zamm_yin::tao::archetype::{ArchetypeTrait, AttributeArchetype};
+use zamm_yin::tao::form::{Form, FormTrait};
+use zamm_yin::tao::relation::attribute::{Attribute, AttributeTrait};
+use zamm_yin::tao::YIN_MAX_ID;
+use zamm_yin::Wrapper;
+
+/// Name the concept actually took on when implemented.
+#[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ImplementationName {
+    base: FinalNode,
+}
+
+impl Debug for ImplementationName {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        debug_wrapper("ImplementationName", self, f)
+    }
+}
+
+impl From<usize> for ImplementationName {
+    fn from(id: usize) -> Self {
+        Self {
+            base: FinalNode::from(id),
+        }
+    }
+}
+
+impl From<FinalNode> for ImplementationName {
+    fn from(f: FinalNode) -> Self {
+        Self { base: f }
+    }
+}
+
+impl<'a> TryFrom<&'a str> for ImplementationName {
+    type Error = String;
+
+    fn try_from(name: &'a str) -> Result<Self, Self::Error> {
+        FinalNode::try_from(name).map(|f| Self { base: f })
+    }
+}
+
+impl Wrapper for ImplementationName {
+    type BaseType = FinalNode;
+
+    fn essence(&self) -> &FinalNode {
+        &self.base
+    }
+
+    fn essence_mut(&mut self) -> &mut FinalNode {
+        &mut self.base
+    }
+}
+
+impl<'a> ArchetypeTrait<'a> for ImplementationName {
+    type ArchetypeForm = AttributeArchetype;
+    type Form = ImplementationName;
+
+    const TYPE_ID: usize = YIN_MAX_ID + 13;
+    const TYPE_NAME: &'static str = "implementation-name";
+    const PARENT_TYPE_ID: usize = Attribute::TYPE_ID;
+}
+
+impl FormTrait for ImplementationName {}
+
+impl AttributeTrait for ImplementationName {
+    type OwnerForm = Form;
+    type ValueForm = Form;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tao::initialize_kb;
+    use std::rc::Rc;
+    use zamm_yin::node_wrappers::CommonNodeTrait;
+    use zamm_yin::tao::archetype::ArchetypeFormTrait;
+    use zamm_yin::tao::form::FormTrait;
+    use zamm_yin::tao::relation::attribute::{Owner, Value};
+    use zamm_yin::tao::Tao;
+
+    #[test]
+    fn check_type_created() {
+        initialize_kb();
+        #[rustfmt::skip]
+        assert_eq!(ImplementationName::archetype().id(), ImplementationName::TYPE_ID);
+        assert_eq!(
+            ImplementationName::archetype().internal_name(),
+            Some(Rc::new(ImplementationName::TYPE_NAME.to_string()))
+        );
+    }
+
+    #[test]
+    fn check_type_attributes() {
+        initialize_kb();
+        #[rustfmt::skip]
+        assert_eq!(ImplementationName::archetype().introduced_attribute_archetypes(), vec![]);
+        #[rustfmt::skip]
+        assert_eq!(ImplementationName::archetype().attribute_archetypes(), vec![Owner::archetype(), Value::archetype()]);
+    }
+
+    #[test]
+    fn from_node_id() {
+        initialize_kb();
+        let concept = ImplementationName::individuate();
+        let concept_copy = ImplementationName::from(concept.id());
+        assert_eq!(concept.id(), concept_copy.id());
+    }
+
+    #[test]
+    fn from_name() {
+        initialize_kb();
+        let mut concept = ImplementationName::individuate();
+        concept.set_internal_name("A".to_owned());
+        #[rustfmt::skip]
+        assert_eq!(ImplementationName::try_from("A").map(|c| c.id()), Ok(concept.id()));
+        assert!(ImplementationName::try_from("B").is_err());
+    }
+
+    #[test]
+    fn test_wrapper_implemented() {
+        initialize_kb();
+        let concept = ImplementationName::individuate();
+        assert_eq!(concept.essence(), &FinalNode::from(concept.id()));
+    }
+
+    #[test]
+    fn check_attribute_constraints() {
+        initialize_kb();
+        assert_eq!(
+            ImplementationName::archetype().owner_archetype(),
+            Tao::archetype().as_archetype()
+        );
+        assert_eq!(
+            ImplementationName::archetype().value_archetype(),
+            Tao::archetype().as_archetype()
+        );
+    }
+
+    #[test]
+    fn get_owner() {
+        initialize_kb();
+        let mut instance = ImplementationName::individuate();
+        let owner_of_instance = Tao::individuate();
+        instance.set_owner(&owner_of_instance);
+        assert_eq!(instance.owner(), Some(owner_of_instance));
+        assert_eq!(instance.value(), None);
+    }
+
+    #[test]
+    fn get_value() {
+        initialize_kb();
+        let mut instance = ImplementationName::individuate();
+        let value_of_instance = Tao::individuate();
+        instance.set_value(&value_of_instance);
+        assert_eq!(instance.owner(), None);
+        assert_eq!(instance.value(), Some(value_of_instance));
+    }
+}
---
+++ src/tao/relation/attribute/rust_primitive_form.rs
@@ -0,0 +1,160 @@
+use std::convert::TryFrom;
+use std::fmt;
+use std::fmt::{Debug, Formatter};
+use zamm_yin::node_wrappers::{debug_wrapper, FinalNode};
+use zamm_yin::tao::archetype::{ArchetypeTrait, AttributeArchetype};
+use zamm_yin::tao::form::{Form, FormTrait};
+use zamm_yin::tao::relation::attribute::{Attribute, AttributeTrait};
+use zamm_yin::tao::YIN_MAX_ID;
+use zamm_yin::Wrapper;
+
+/// The Rust primitive that a Yin data concept is implemented by.
+#[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct RustPrimitive {
+    base: FinalNode,
+}
+
+impl Debug for RustPrimitive {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        debug_wrapper("RustPrimitive", self, f)
+    }
+}
+
+impl From<usize> for RustPrimitive {
+    fn from(id: usize) -> Self {
+        Self {
+            base: FinalNode::from(id),
+        }
+    }
+}
+
+impl From<FinalNode> for RustPrimitive {
+    fn from(f: FinalNode) -> Self {
+        Self { base: f }
+    }
+}
+
+impl<'a> TryFrom<&'a str> for RustPrimitive {
+    type Error = String;
+
+    fn try_from(name: &'a str) -> Result<Self, Self::Error> {
+        FinalNode::try_from(name).map(|f| Self { base: f })
+    }
+}
+
+impl Wrapper for RustPrimitive {
+    type BaseType = FinalNode;
+
+    fn essence(&self) -> &FinalNode {
+        &self.base
+    }
+
+    fn essence_mut(&mut self) -> &mut FinalNode {
+        &mut self.base
+    }
+}
+
+impl<'a> ArchetypeTrait<'a> for RustPrimitive {
+    type ArchetypeForm = AttributeArchetype;
+    type Form = RustPrimitive;
+
+    const TYPE_ID: usize = YIN_MAX_ID + 14;
+    const TYPE_NAME: &'static str = "rust-primitive";
+    const PARENT_TYPE_ID: usize = Attribute::TYPE_ID;
+}
+
+impl FormTrait for RustPrimitive {}
+
+impl AttributeTrait for RustPrimitive {
+    type OwnerForm = Form;
+    type ValueForm = Form;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tao::initialize_kb;
+    use std::rc::Rc;
+    use zamm_yin::node_wrappers::CommonNodeTrait;
+    use zamm_yin::tao::archetype::ArchetypeFormTrait;
+    use zamm_yin::tao::form::FormTrait;
+    use zamm_yin::tao::relation::attribute::{Owner, Value};
+    use zamm_yin::tao::Tao;
+
+    #[test]
+    fn check_type_created() {
+        initialize_kb();
+        assert_eq!(RustPrimitive::archetype().id(), RustPrimitive::TYPE_ID);
+        assert_eq!(
+            RustPrimitive::archetype().internal_name(),
+            Some(Rc::new(RustPrimitive::TYPE_NAME.to_string()))
+        );
+    }
+
+    #[test]
+    fn check_type_attributes() {
+        initialize_kb();
+        #[rustfmt::skip]
+        assert_eq!(RustPrimitive::archetype().introduced_attribute_archetypes(), vec![]);
+        #[rustfmt::skip]
+        assert_eq!(RustPrimitive::archetype().attribute_archetypes(), vec![Owner::archetype(), Value::archetype()]);
+    }
+
+    #[test]
+    fn from_node_id() {
+        initialize_kb();
+        let concept = RustPrimitive::individuate();
+        let concept_copy = RustPrimitive::from(concept.id());
+        assert_eq!(concept.id(), concept_copy.id());
+    }
+
+    #[test]
+    fn from_name() {
+        initialize_kb();
+        let mut concept = RustPrimitive::individuate();
+        concept.set_internal_name("A".to_owned());
+        #[rustfmt::skip]
+        assert_eq!(RustPrimitive::try_from("A").map(|c| c.id()), Ok(concept.id()));
+        assert!(RustPrimitive::try_from("B").is_err());
+    }
+
+    #[test]
+    fn test_wrapper_implemented() {
+        initialize_kb();
+        let concept = RustPrimitive::individuate();
+        assert_eq!(concept.essence(), &FinalNode::from(concept.id()));
+    }
+
+    #[test]
+    fn check_attribute_constraints() {
+        initialize_kb();
+        assert_eq!(
+            RustPrimitive::archetype().owner_archetype(),
+            Tao::archetype().as_archetype()
+        );
+        assert_eq!(
+            RustPrimitive::archetype().value_archetype(),
+            Tao::archetype().as_archetype()
+        );
+    }
+
+    #[test]
+    fn get_owner() {
+        initialize_kb();
+        let mut instance = RustPrimitive::individuate();
+        let owner_of_instance = Tao::individuate();
+        instance.set_owner(&owner_of_instance);
+        assert_eq!(instance.owner(), Some(owner_of_instance));
+        assert_eq!(instance.value(), None);
+    }
+
+    #[test]
+    fn get_value() {
+        initialize_kb();
+        let mut instance = RustPrimitive::individuate();
+        let value_of_instance = Tao::individuate();
+        instance.set_value(&value_of_instance);
+        assert_eq!(instance.owner(), None);
+        assert_eq!(instance.value(), Some(value_of_instance));
+    }
+}
