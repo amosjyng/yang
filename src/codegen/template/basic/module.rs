0dfe7839a3489cb36d803d3e4719bd47e25943c9
Amos Jun-yeung Ng <me@amos.ng>
2020-11-19 21:40:06 -0800

    Generate modules that aren't test modules

--- src/codegen/template/basic/module.rs
+++ src/codegen/template/basic/module.rs
@@ -7,6 +7,7 @@ use std::rc::Rc;
 /// Fragment for a module declaration.
 pub struct ModuleFragment {
     name: String,
+    test: bool,
     content: Rc<RefCell<AppendedFragment>>,
 }
 
@@ -15,13 +16,23 @@ impl ModuleFragment {
     pub fn new(name: String) -> Self {
         Self {
             name,
+            test: false,
             content: Rc::new(RefCell::new(AppendedFragment::default())),
         }
     }
 
     /// Create a new module with the name `tests`.
     pub fn new_test_module() -> Self {
-        Self::new("tests".to_owned())
+        Self {
+            name: "tests".to_owned(),
+            test: true,
+            content: Rc::new(RefCell::new(AppendedFragment::default())),
+        }
+    }
+
+    /// Mark a module as being a test module.
+    pub fn mark_as_test(&mut self) {
+        self.test = true;
     }
 
     /// Add a fragment to the internals of this module.
@@ -33,9 +44,13 @@ impl ModuleFragment {
 impl CodeFragment for ModuleFragment {
     fn body(&self) -> String {
         let mut imports = self.content.borrow().imports();
-        imports.push("super::*".to_owned());
+        if self.test {
+            imports.push("super::*".to_owned());
+        }
         let imports_str =
             imports_as_str(&imports.iter().map(|s| s.as_str()).collect::<Vec<&str>>());
+
+        let cfg_test = if self.test { "#[cfg(test)]" } else { "" };
         let internal_code = format!("{}\n\n{}\n", imports_str, self.content.borrow().body());
         let internals = AtomicFragment {
             imports: Vec::new(),
@@ -44,9 +59,10 @@ impl CodeFragment for ModuleFragment {
         let nested = NestedFragment {
             imports: Vec::new(),
             preamble: formatdoc! {"
-                #[cfg(test)]
+                {cfg_test}
                 mod {name} {{",
                 name = self.name,
+                cfg_test = cfg_test,
             },
             nesting: Some(Rc::new(RefCell::new(internals))),
             postamble: "}".to_owned(),
@@ -64,6 +80,30 @@ mod tests {
     use super::super::AtomicFragment;
     use super::*;
     use indoc::indoc;
+
+    #[test]
+    fn test_regular_module() {
+        let mut test_mod = ModuleFragment::new("MyMod".to_owned());
+        test_mod.append(Rc::new(RefCell::new(AtomicFragment {
+            imports: Vec::new(),
+            atom: indoc! {r#"
+                fn a() {
+                    println!("actually b");
+                }"#}
+            .to_string(),
+        })));
+
+        assert_eq!(test_mod.imports(), Vec::<String>::new());
+        assert_eq!(
+            test_mod.body(),
+            indoc! {r#"
+                mod MyMod {
+                    fn a() {
+                        println!("actually b");
+                    }
+                }"#}
+        );
+    }
 
     #[test]
     fn test_file_with_tests() {
