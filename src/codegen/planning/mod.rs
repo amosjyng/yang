c4573c1b5ae37f9c1c7e1cb5b98764f932728012
Amos Jun-yeung Ng <me@amos.ng>
2020-11-20 23:46:41 -0800

    Refactor planning module into multiple files

---
+++ src/codegen/planning/mod.rs
@@ -0,0 +1,13 @@
+mod archetype;
+mod id;
+mod imports;
+mod init;
+mod module;
+
+use id::grab_new_implementation_id;
+use imports::{ancestor_path, concept_to_struct, in_own_submodule};
+
+pub use archetype::code_archetype;
+pub use imports::{archetype_file_path, module_file_path};
+pub use init::handle_init;
+pub use module::code_module;
