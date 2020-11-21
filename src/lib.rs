ca7af1252473e2f2d56722e228083d5cfb055ec0
Amos Jun-yeung Ng <me@amos.ng>
2020-11-20 23:57:14 -0800

    Document MVC codegen analogy

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,13 @@
 #![warn(missing_docs)]
 
 /// Code generation utilities.
+///
+/// This is roughly analogous to the MVC framework: Yin concepts are the model, templates are the
+/// view, and the planning modules are the controllers. This is perhaps not too surprising, because
+/// HTML is code too, webservers are code generators for the HTML language. Right now, we're merely
+/// rendering higher-level concepts in the form of Rust code instead of HTML code, and we're using
+/// the Rust compiler instead of the browser to render the generated code in a format that the end
+/// user finds useful.
 pub mod codegen;
 /// Helper functions to make codegen specification smoother.
 pub mod helper;
