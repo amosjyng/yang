9f04b804b100e71cdc983f30374cededa09b71cb
Amos Jun-yeung Ng <me@amos.ng>
2020-11-26 20:06:44 -0800

    Fix self-import introduced by flag functions

--- src/codegen/template/concept/tao.rs
+++ src/codegen/template/concept/tao.rs
@@ -112,7 +112,6 @@ fn tao_fragment(cfg: &TaoConfig) -> AtomicFragment {
     if let Some(import) = &cfg.imports {
         imports.push(import.clone());
     }
-    imports.retain(|i| i != &cfg.this.import); // don't import references to self
 
     AtomicFragment {
         imports,
@@ -253,6 +252,7 @@ fn tao_test_fragment(cfg: &TaoConfig) -> AtomicFragment {
 /// Returns a file fragment, which may be appended to further.
 pub fn tao_file_fragment(cfg: &TaoConfig) -> FileFragment {
     let mut file = FileFragment::default();
+    file.set_self_import(cfg.this.import.clone());
     file.append(Rc::new(RefCell::new(tao_fragment(cfg))));
     file.append_test(Rc::new(RefCell::new(tao_test_fragment(cfg))));
     file
