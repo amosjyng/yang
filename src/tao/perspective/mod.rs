bcb2d9326e47c0bab83403fd6a55d6e64c44c9cb
Amos Jun-yeung Ng <me@amos.ng>
2020-11-28 02:22:45 -0800

    Revert "Creating release v0.1.6"
    This reverts commit 596cf83bc9b30f7174b11f99c3391f65b7c70209.

--- src/tao/perspective/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Perspectives on the world.
-
-mod knowledge_graph_node_form;
-mod perspective_form;
-
-pub use knowledge_graph_node_form::KnowledgeGraphNode;
-pub use perspective_form::Perspective;
