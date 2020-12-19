9a361e40ba8dc53ab22d48ab814e3eaa3edc0761
Amos Jun-yeung Ng <me@amos.ng>
2020-12-19 13:13:02 -0800

    Only re-export imported Yin concepts, and not the Yin modules themselves

--- src/codegen/planning/module.rs
+++ src/codegen/planning/module.rs
@@ -1,8 +1,9 @@
-use super::{ancestor_path, in_own_submodule};
+use super::imports::{ancestor_path, import_path};
+use super::in_own_submodule;
 use crate::codegen::template::concept::archetype_module::{
     code_archetype_module, ArchetypeModuleConfig,
 };
-use crate::tao::form::{Module, ModuleExtension};
+use crate::tao::form::{Crate, CrateExtension, Module, ModuleExtension};
 use crate::tao::perspective::{BuildInfo, BuildInfoExtension, KnowledgeGraphNode};
 use crate::tao::Implement;
 use std::rc::Rc;
@@ -16,9 +17,10 @@ pub fn code_module(request: Implement, module: Module, parent: Archetype) -> Str
     let mut private_submodules = vec![];
     let mut re_exports = vec![];
 
-    if KnowledgeGraphNode::from(parent.id()).is_newly_defined() {
+    let parent_node = KnowledgeGraphNode::from(parent.id());
+    if parent_node.is_newly_defined() {
         archetype_names.push(parent.internal_name_str().unwrap());
-    } else {
+    } else if parent_node.is_imported() {
         // Parent is already defined as part of a dependency, we're only creating this crate so
         // that we can access the children as well. In which case, we should also re-export the
         // concepts defined in the dependency, so that the end consumer does not depend directly on
@@ -41,9 +43,21 @@ pub fn code_module(request: Implement, module: Module, parent: Archetype) -> Str
         // the current Yang depends on, so we can't just rely on the build information stored in
         // the Yin dependency.
         //
-        // Instead, this needs to be done properly as part of an import of Yin's `yin.md` file from
-        // within Yang's `yin.md` file. Only then will the diff between Yin and Yang be accurate.
-        re_exports.push(format!("zamm_yin::{}::*", ancestor_path(&parent, "::")));
+        // Instead, this needs to be a re-export of the *currently* imported Yin concepts. Note
+        // that for this to be successful, the newly defined concepts have to be children of the
+        // imported concepts, not children of Yang's initialized concepts from a previous build.
+        if Crate::yang().version_at_least(0, 1, 8) {
+            re_exports.push(import_path(&parent_node, false));
+            for child in parent.child_archetypes() {
+                let child_node = KnowledgeGraphNode::from(child.id());
+                if child_node.is_imported() && !in_own_submodule(&child) {
+                    re_exports.push(import_path(&child_node, false));
+                }
+            }
+        } else {
+            // backwards compatible functionality
+            re_exports.push(format!("zamm_yin::{}::*", ancestor_path(&parent, "::")));
+        }
     }
 
     for child in parent.child_archetypes() {
@@ -58,7 +72,7 @@ pub fn code_module(request: Implement, module: Module, parent: Archetype) -> Str
         } else if KnowledgeGraphNode::from(child.id()).is_newly_defined() {
             archetype_names.push(child.internal_name_str().unwrap());
         } // else, if this child doesn't have their own module, and has also been already defined,
-          // then we will re-export them later in this function
+          // then we will have already set them for re-export earlier
     }
 
     for submodule in module.submodules() {
