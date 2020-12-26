8e7a97cb24edf1e5a29756bb0206403bed66e874
Amos Jun-yeung Ng <me@amos.ng>
2020-12-26 15:00:49 -0800

    Revert "Creating release v0.1.8"
    This reverts commit 6f057de74b9e9cbff67fa31e24440430badccb72.

--- src/tao/archetype/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Types of forms, as opposed to the forms themselves.
-
-mod attribute_activation;
-mod create_implementation;
-
-pub use attribute_activation::CodegenFlags;
-pub use create_implementation::CreateImplementation;
-pub use zamm_yin::tao::archetype::*;
