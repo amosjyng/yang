df5ae49193aa8f3422b5f8813fb73382f2f41646
Amos Jun-yeung Ng <me@amos.ng>
2021-01-27 01:43:44 -0800

    Generate code for associated types inside of a trait

--- src/codegen/template/basic/trait_fragment.rs
+++ src/codegen/template/basic/trait_fragment.rs
@@ -1,17 +1,17 @@
 use super::{
     Appendable, AppendedFragment, AtomicFragment, CodeFragment, ItemDeclaration,
-    ItemDeclarationAPI, TypeFragment,
+    ItemDeclarationAPI, TypeFragment, TypeDeclaration,
 };
 use std::cell::RefCell;
 use std::rc::Rc;
 
-/// Fragment for a trait.
+/// Fragment for generating a Rust `Trait`.
 pub struct TraitFragment {
-    ///  New type that this trait defines.
+    ///  New type that this `Trait` defines.
     trait_type: TypeFragment,
-    /// Declaration fragment for this trait.
+    /// Declaration fragment for this `Trait`.
     declaration: ItemDeclaration,
-    /// Actual internal code fragments for the trait.
+    /// Actual internal code fragments for the `Trait`.
     content: Rc<RefCell<AppendedFragment>>,
 }
 
@@ -27,6 +27,11 @@ impl TraitFragment {
     /// Add a trait requirement
     pub fn add_required_trait(&mut self, required_trait: Box<dyn CodeFragment>) {
         self.trait_type.add_required_trait(required_trait);
+    }
+
+    /// Add an associated type to this `Trait`.
+    pub fn add_associated_type(&mut self, associated_type: TypeDeclaration) {
+        self.content.borrow_mut().prepend(Rc::new(RefCell::new(associated_type)));
     }
 }
 
@@ -170,6 +175,44 @@ mod tests {
                     fn do_something(&mut self) {
                         self.bar_capability();
                         self.baz_capability();
+                    }
+                }"}
+        );
+    }
+
+    #[test]
+    fn test_associated_types() {
+        let mut f = TraitFragment::new("Foo".to_owned());
+        f.add_required_trait(Box::new(AtomicFragment {
+            atom: "Bar".to_owned(),
+            imports: vec!["crate::Bar".to_owned()],
+        }));
+        f.add_required_trait(Box::new(AtomicFragment {
+            atom: "Baz".to_owned(),
+            imports: vec!["crate::Baz".to_owned()],
+        }));
+        f.append(Rc::new(RefCell::new(AtomicFragment {
+            imports: vec![],
+            atom: indoc! {"
+                fn do_something(&mut self) -> Self::Tendies {
+                    self.bar_capability();
+                    self.baz_yolo_fd()
+                }"}
+            .to_string(),
+        })));
+        let mut tendies = TypeDeclaration::new("Tendies".to_owned());
+        tendies.add_required_trait(Box::new(TypeFragment::new("GME".to_owned())));
+        f.add_associated_type(tendies);
+
+        assert_eq!(
+            f.body(80),
+            indoc! {"
+                trait Foo: Bar + Baz {
+                    type Tendies: GME;
+
+                    fn do_something(&mut self) -> Self::Tendies {
+                        self.bar_capability();
+                        self.baz_yolo_fd()
                     }
                 }"}
         );
